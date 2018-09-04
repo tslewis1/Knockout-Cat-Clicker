ef13c3f3d204965c16debcd081bf305735886d0b
Taryn Lewis <tarynslewis@gmail.com>
2018-09-04 12:57:36 -0700

    Move code around to create new Cat

--- js/app.js
+++ js/app.js
@@ -1,19 +1,40 @@
-var ViewModel = function() {
+var Cat = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Smiley');
 	this.imgSrc = ko.observable('img/Smiling_Cats_17.jpg');
 	this.imgAttribution = ko.observable('https://kittentoob.com/wp-content/uploads/2013/04/Smiling_Cats_17.jpg');
+	this.nicknames = ko.observableArray(['Smiley Miley', 'Kitty Wampus', 'Miss Whiskerson']);
+
+
+	this.title = ko.computed(function() {
+		let title;
+		let clicks = this.clickCount();
+		if (clicks < 15) {
+			title = 'Newborn';
+		} else if (clicks < 45) {
+			title = 'Infant';
+		} else if (clicks < 90) {
+			title = 'Child';
+		} else if (clicks < 150) {
+			title = 'Teen';
+		} else if (clicks < 300) {
+			title = 'Adult';
+		} else {
+			title = 'Unicorn';
+		}
+
+		return title;
+	}, this);
+
+}
+
+var ViewModel = function() {
+
+	this.currentCat = ko.observable(new Cat()); 
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
-
-		let count = 0; 
-		count++;
 	};
-
-	this.nicknames = ko.observableArray(['Smiley Miley', 'Kitty Wampus', 'Miss Whiskerson']);
-
-	this.
 }
 
 ko.applyBindings(new ViewModel());
